c01cd2a2a1f433f8059710faf32588532dfd19f9
SengSerany <cranyseng@gmail.com>
2019-09-19 01:15:30 +0200

    create pages for the cart

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :carts, except: [:index, :new, :edit]
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
