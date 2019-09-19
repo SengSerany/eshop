5d167f233889fa269517264763b813f701fbd617
SengSerany <cranyseng@gmail.com>
2019-09-19 10:46:24 +0200

    create a link between cart and product, we will be able to add a product to a cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_011415) do
+ActiveRecord::Schema.define(version: 2019_09_19_084250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2019_09_19_011415) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
 
   create_table "carts", force: :cascade do |t|
@@ -57,5 +66,7 @@ ActiveRecord::Schema.define(version: 2019_09_19_011415) do
   end
 
   add_foreign_key "accounts", "users"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "items"
   add_foreign_key "carts", "users"
 end
