2e2f4dd29eeba72087421a16bf0a473a00d54104
SengSerany <cranyseng@gmail.com>
2019-09-19 04:34:16 +0200

    admin can add new product to sell on the website

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
+    @items = Item.all
   end
 
   def show
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(permit_item)
+    if @item.save
+      redirect_to @item
+    end
   end
 
   def edit
@@ -19,5 +26,15 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def permit_item
+     params.require(:item).permit(:name, :description, :price)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
