8c737e82d139b9c5df9098ab499f17d647a3a9f1
SengSerany <cranyseng@gmail.com>
2019-09-19 04:06:01 +0200

    add customer infos on profil

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,5 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update, :destroy]
   def show
   end
 
@@ -9,8 +10,20 @@ class AccountsController < ApplicationController
   end
 
   def update
+    @account.update(permit_account)
+    redirect_to @account
   end
 
   def destroy
+  end
+
+  private
+
+  def permit_account
+     params.require(:account).permit(:first_name, :last_name, :street, :zipcode, :city, :stripe_customer_id)
+  end
+
+  def set_account
+    @account = Account.find(params[:id])
   end
 end
